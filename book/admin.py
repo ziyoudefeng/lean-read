12ca41f9368d9349084c31e24cefafa56839e614
JackonYang <jiekunyang@gmail.com>
2013-10-05 21:24:55 +0800

    add 2 models, chapter and reading log

--- book/admin.py
+++ book/admin.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8-*-
 from django.contrib import admin
-from book.models import Book
+from book.models import Book, Chapter, ReadingLog
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'cnFile', 'enFile')
 
 
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('book', 'chapNo', 'chapName')
+
+
+class ReadingLogAdmin(admin.ModelAdmin):
+    list_display = ('chapter', 'reader', 'mark', 'notes')
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Chapter, ChapterAdmin)
+admin.site.register(ReadingLog, ReadingLogAdmin)
--- book/models.py
+++ book/models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8-*-
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 
 class Book(models.Model):
@@ -11,3 +13,21 @@ class Book(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Chapter(models.Model):
+    chapId = models.AutoField(primary_key=True)
+    book = models.ForeignKey(Book)
+    chapNo = models.IntegerField('章节序号')
+    chapName = models.CharField('章节名', max_length=100, blank=True)
+
+    def __unicode__(self):
+        return '%s-Chap %s' % (self.book, self.chapNo)
+
+
+class ReadingLog(models.Model):
+    chapter = models.ForeignKey(Chapter)
+    reader = models.ForeignKey(User)
+    mark = models.IntegerField('评分')
+    notes = models.CharField('评论', max_length=500, blank=True)
+    timestamp = models.DateTimeField('时间', default=datetime.datetime.now)
