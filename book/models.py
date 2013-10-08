c7b6c33e45e3699c5460fd245a358b2903b3f33b
JackonYang <jiekunyang@gmail.com>
2013-10-08 20:13:12 +0800

    remove authors from books model

--- book/models.py
+++ book/models.py
@@ -7,7 +7,6 @@ import datetime
 class Book(models.Model):
     bookId = models.AutoField(primary_key=True)
     name = models.CharField('书名', max_length=100)
-    author = models.CharField('作者', max_length=100, blank=True)
     cnFile = models.FileField(upload_to='books/', blank=True)
     enFile = models.FileField(upload_to='books/', blank=True)
 
