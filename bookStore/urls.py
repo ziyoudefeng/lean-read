5fd6e629baeced870c7fac8fe55628bb1021b08d
JackonYang <jiekunyang@gmail.com>
2013-10-09 23:11:12 +0800

    simplify url code in bookStrore

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-    url(r'^$', 'bookStore.views.book_list'),
-    url(r'^(\d+)$', 'bookStore.views.book_detail'),
+urlpatterns = patterns('bookStore.views',
+    url(r'^$', 'book_list'),
+    url(r'^(\d+)$', 'book_detail'),
 )
