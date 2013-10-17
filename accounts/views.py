c4b9a1cf81ddda8c19bc1f56ff8ac9d7f203e51f
JackonYang <jiekunyang@gmail.com>
2013-10-18 02:34:54 +0800

    mock profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.http import HttpResponse
 
 
-def login(request):
-    return render_to_response('login.html', RequestContext(request))
+def profile(request):
+    return HttpResponse('profile page')
