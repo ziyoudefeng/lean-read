65e1dff8e71f5f7f4fd26708a25f6f7e0f3613dd
JackonYang <jiekunyang@gmail.com>
2013-10-17 21:03:58 +0800

    remove admin urls from bookstore urls.py

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import patterns, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
 
 urlpatterns = patterns('bookStore.views',
     url(r'^$', 'book_list'),
